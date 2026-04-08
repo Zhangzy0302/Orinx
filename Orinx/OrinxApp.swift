a77f9890f35bbb9cbd4248835cb25af785d00469
zzy <2218847957@qq.com>
2026-04-08 15:32:26 +0800

    Initial Commit

---
+++ Orinx/OrinxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrinxApp.swift
+//  Orinx
+//
+//  Created by yangyang on 2026/4/8.
+//
+
+import SwiftUI
+
+@main
+struct OrinxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
